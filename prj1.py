751956f43b2045dd90f0bd68bbd1fa5e87b8ed4b
moin532 <122919446+moin532@users.noreply.github.com>
2023-03-09 22:25:08 +0530

    Add files via upload
     project1 taking input from dictionary ...PYTHON

---
+++ prj1.py
@@ -0,0 +1,9 @@
+                       #Dictionary
+
+d1 = {
+    "1 cheese" , "2 burger" ,"3 samosa"}
+print(d1)
+
+print("enter your fav dish\n")
+d2 = input()
+print("you entered your fav dish 1\n", d2, d1);
